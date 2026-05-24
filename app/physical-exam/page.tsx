5f56e44e38a0d30d8a0c826ffe4eecc0e22fa1e7
LoLyeah <secretrizqi@ymail.com>
2026-05-24 16:46:46 +0700

    chore: upgrade Next.js and peer dependencies to 16.2.6

--- app/physical-exam/page.tsx
+++ app/physical-exam/page.tsx
@@ -25,6 +25,7 @@ export default function PhysicalExamPage() {
   // Mounted status for portal
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
